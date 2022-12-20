f781526a72a6db902f63b5205f6fc0cb90cbba9c
hubertolafaille <120303136+hubertolafaille@users.noreply.github.com>
2022-12-20 07:18:57 +0100

    Initial commit

---
+++ src/test/java/io/github/hubertolafaille/warwickapi/WarwickapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hubertolafaille.warwickapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarwickapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
